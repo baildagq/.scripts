62e497e02315b7bc6b44c17b9b3a6ebb1bec41ac
qiang.qi <qiang.qi@realai.ai>
2023-08-01 17:22:50 +0800

    update

--- looprun.sh
+++ looprun.sh
@@ -3,6 +3,10 @@
 flag=1
 while [ $flag -ne 0 ]
 do
-    sudo pacman -S joplin-desktop-electron --noconfirm
+    # sudo pacman -S joplin-desktop-electron --noconfirm
+    # pacman -S telegram-desktop cuda-tools --noconfirm
+    # yay -Syyu --noconfirm
+    # ssh 136 && sleep 10
+    sudo pacman -Syyu --noconfirm
     flag=$?
 done
--- new-machine/zsh.sh
+++ new-machine/zsh.sh
@@ -1,5 +1,12 @@
 # zsh
-sh -c "$(wget -O- https://ghproxy.com/https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+set -o errexit
+
+# sh -c "$(wget -O- https://ghproxy.com/https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf ~/.oh-my-zsh
+wget https://ghproxy.com/https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sed -i "s@https://github@https://ghproxy.com/https://github@g" install.sh
+bash install.sh
 git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions  
 git clone https://ghproxy.com/https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 
